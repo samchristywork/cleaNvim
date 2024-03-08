92551ebc835792c704beee6d74c4277ef1a835ff
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-03-08 15:14:24 -0600

    feat: Add package list to cleaNvim
    These packages are referenced in the config files, and need to be
    present to run this distribution.

---
+++ install.sh
@@ -0,0 +1,26 @@
+# See <https://github.com/samchristywork/stopm>, or install these plugins using
+# your favorite plugin manager.
+
+./stopm.sh akinsho bufferline.nvim
+./stopm.sh chrisbra unicode.vim
+./stopm.sh folke tokyonight.nvim
+./stopm.sh github copilot.vim
+./stopm.sh godlygeek tabular
+./stopm.sh hrsh7th cmp-nvim-lsp
+./stopm.sh hrsh7th nvim-cmp
+./stopm.sh kepano flexoki-neovim
+./stopm.sh L3MON4D3 LuaSnip
+./stopm.sh lewis6991 gitsigns.nvim
+./stopm.sh neovim nvim-lspconfig
+./stopm.sh nvim-lualine lualine.nvim
+./stopm.sh nvim-lua plenary.nvim
+./stopm.sh nvim-telescope telescope.nvim
+./stopm.sh nvim-tree nvim-web-devicons
+./stopm.sh nvim-treesitter nvim-treesitter
+./stopm.sh nyoom-engineering oxocarbon.nvim
+./stopm.sh petertriho nvim-scrollbar
+./stopm.sh preservim tagbar
+./stopm.sh saadparwaiz1 cmp_luasnip
+./stopm.sh stevearc oil.nvim
+./stopm.sh williamboman mason-lspconfig.nvim
+./stopm.sh williamboman mason.nvim
