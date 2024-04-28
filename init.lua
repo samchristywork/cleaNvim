ef5d282f229c3d36d08b6cf91f71b9fe5da345cd
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-04-28 15:41:29 -0500

    feat: Add code block to save cursor position
    This code block move the cursor back to the place where it was the last
    time the file was open.

--- init.lua
+++ init.lua
@@ -4,6 +4,12 @@ require("plugins")
 require("lsp")
 require("snippets")
 
+-- Go back to the last cursor position when a file is opened
+vim.api.nvim_create_autocmd({'BufWinEnter'}, {
+  pattern = '*',
+  command = 'silent! normal! g`"zv',
+})
+
 vim.g.copilot_filetypes = { markdown = false }
 vim.opt.completeopt = "menu,menuone,noselect"                     -- menuone is default, menu is for showing the menu even if there is only one match, noselect is for not selecting the first match
 vim.opt.dictionary = vim.opt.dictionary + "/usr/share/dict/words" -- In insert mode, C-X, C-K will bring up the completion list if omnifunc is enabled
