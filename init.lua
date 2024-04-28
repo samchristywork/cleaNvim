fac4e211934bcba93f75943851f9decfb1940f3f
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-04-28 16:09:40 -0500

    chore: Add special formatting options
    This change is to get rid of the comment continuation options that are
    present in default nvim.

--- init.lua
+++ init.lua
@@ -16,6 +16,14 @@ vim.api.nvim_create_autocmd({'BufWinEnter'}, {
   command = 'setlocal noexpandtab',
 })
 
+-- Set formatoptions to tql for all files
+vim.api.nvim_create_autocmd({'BufWinEnter'}, {
+  pattern = '*',
+  callback = function()
+    vim.opt.formatoptions = 'tql';
+  end
+})
+
 vim.g.copilot_filetypes = { markdown = false }
 vim.opt.completeopt = "menu,menuone,noselect"                     -- menuone is default, menu is for showing the menu even if there is only one match, noselect is for not selecting the first match
 vim.opt.dictionary = vim.opt.dictionary + "/usr/share/dict/words" -- In insert mode, C-X, C-K will bring up the completion list if omnifunc is enabled
