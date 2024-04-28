b12732b5dd5f26d54e6c5721f08fcf30905c2a05
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-04-28 15:42:53 -0500

    chore: Add special format options for Go files
    Go is stupid and uses tabs instead of spaces, so I need to add special
    formatting options in my config.

--- init.lua
+++ init.lua
@@ -10,6 +10,12 @@ vim.api.nvim_create_autocmd({'BufWinEnter'}, {
   command = 'silent! normal! g`"zv',
 })
 
+-- Allow tabs in Go files
+vim.api.nvim_create_autocmd({'BufWinEnter'}, {
+  pattern = '*.go',
+  command = 'setlocal noexpandtab',
+})
+
 vim.g.copilot_filetypes = { markdown = false }
 vim.opt.completeopt = "menu,menuone,noselect"                     -- menuone is default, menu is for showing the menu even if there is only one match, noselect is for not selecting the first match
 vim.opt.dictionary = vim.opt.dictionary + "/usr/share/dict/words" -- In insert mode, C-X, C-K will bring up the completion list if omnifunc is enabled
