fbeb0271df5c60da10507ed5da6d9fb280782f9b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-09-22 19:47:34 -0500

    feat: Add diagnostic messages on buffer changes
    This uses the todo and linelength modules that I wrote recently.
    
    Also, disable Copilot by default.

--- init.lua
+++ init.lua
@@ -24,6 +24,33 @@ vim.api.nvim_create_autocmd({'BufWinEnter'}, {
   end
 })
 
-vim.g.copilot_filetypes = { markdown = false }
+vim.g.copilot_filetypes = {
+  markdown = false,
+}
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+  pattern = {"*"},
+  callback = function(ev)
+    vim.cmd('Copilot disable')
+  end
+})
+
+vim.api.nvim_create_autocmd({
+  "BufAdd",
+  "TextChanged",
+  "TextChangedI",
+  "BufEnter",
+  "BufWinEnter",
+  "VimEnter",
+  "BufRead",
+  "BufNewFile"
+}, {
+  pattern = "*",
+  callback = function()
+    require('todo').add_todo_diagnostics()
+    require('linelength').add_line_length_diagnostics()
+  end
+})
+
 vim.opt.completeopt = "menu,menuone,noselect"                     -- menuone is default, menu is for showing the menu even if there is only one match, noselect is for not selecting the first match
 vim.opt.dictionary = vim.opt.dictionary + "/usr/share/dict/words" -- In insert mode, C-X, C-K will bring up the completion list if omnifunc is enabled
